a6842baba707b5e298d47a19425a3f32c26d9f94
Thalles Leonardelli <thalles63@gmail.com>
2026-01-22 13:28:18 -0300

    Chage API to google environment

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    API_URL: "https://trophies-api-bun.onrender.com/api",
+    API_URL: "https://api.inscricoesrevolucao.com.br/api/trophies",
     HLTB_MS_URL: "https://ncvsnm6m5ntc5bbrk6vk7cwzfy0rmadk.lambda-url.sa-east-1.on.aws",
     PRODUCTION_MODE: true
 };
